69de84e30ded9253a889f9de18da7579b928398a
andre_jeon <andre.jeon@balancehero.com>
2019-11-29 14:56:21 +0900

    asdfsf

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
     System.out.println("The current local time is: " + currentTime);
+    System.out.println("The current local time is: " + currentTime);
     
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
